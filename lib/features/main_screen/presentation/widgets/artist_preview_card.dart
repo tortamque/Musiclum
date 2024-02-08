965fc13231338aab1493733df91ea8f5fee50228
tortamque <90132962+tortamque@users.noreply.github.com>
2024-02-08 17:17:23 +0200

    Finish card preview

--- lib/features/main_screen/presentation/widgets/artist_preview_card.dart
+++ lib/features/main_screen/presentation/widgets/artist_preview_card.dart
@@ -10,28 +10,40 @@ class ArtistPreviewCard extends StatelessWidget {
   Widget build(BuildContext context) => Card(
     child: Row(
       children: [
-        ClipOval(
-          child: Image.network(
-            photoUrl,
-            width: 100, 
-            height: 100,
-            fit: BoxFit.cover, 
-            loadingBuilder: (context, child, loadingProgress) {
-              if (loadingProgress == null) {
-                return child;
-              }
-              return Center(
-                child: CircularProgressIndicator(
-                  value: loadingProgress.expectedTotalBytes != null
-                      ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
-                      : null,
-                ),
-              );
-            },
+        Padding(
+          padding: const EdgeInsets.all(15),
+          child: ClipOval(
+            child: Image.network(
+              photoUrl,
+              width: 75, 
+              height: 75,
+              fit: BoxFit.cover, 
+              loadingBuilder: (context, child, loadingProgress) {
+                if (loadingProgress == null) {
+                  return child;
+                }
+                return Center(
+                  child: CircularProgressIndicator(
+                    value: loadingProgress.expectedTotalBytes != null
+                        ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
+                        : null,
+                  ),
+                );
+              },
+            ),
           ),
         ),
-        Text(artistName),
+
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Text(
+            artistName,
+            style: const TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ),
       ],
-    )
+    ),
   );
 }
