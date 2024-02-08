ec31d3701c6c85fd3a10ff3188bb18d521a445fe
tortamque <90132962+tortamque@users.noreply.github.com>
2024-02-08 15:46:56 +0200

    Update models

--- lib/core/shared/data/models/artist_model.dart
+++ lib/core/shared/data/models/artist_model.dart
@@ -13,4 +13,49 @@ class ArtistModel extends ArtistEntity{
     required super.type, 
     required super.uri,
   });
+
+  factory ArtistModel.fromJson(Map<String, dynamic> json) => ArtistModel(
+      externalUrls: ExternalUrlsModel.fromJson(json['external_urls']),
+      followers: FollowersModel.fromJson(json['followers']),
+      genres: List<String>.from(json['genres']),
+      href: json['href'],
+      id: json['id'],
+      images: List<ImageModel>.from(json['images'].map((imageJson) => ImageModel.fromJson(imageJson))),
+      name: json['name'],
+      popularity: json['popularity'],
+      type: json['type'],
+      uri: json['uri'],
+    );
 }
+
+class ExternalUrlsModel extends ExternalUrlsEntity {
+  const ExternalUrlsModel({required String spotify}) : super(spotify: spotify);
+
+  factory ExternalUrlsModel.fromJson(Map<String, dynamic> json) => ExternalUrlsModel(
+      spotify: json['spotify'],
+    );
+}
+
+class FollowersModel extends FollowersEntity {
+  const FollowersModel({String? href, required int total}) : super(href: href, total: total);
+
+  factory FollowersModel.fromJson(Map<String, dynamic> json) => FollowersModel(
+      href: json['href'],
+      total: json['total'],
+    );
+}
+
+class ImageModel extends ImageEntity {
+  const ImageModel({
+    required String url,
+    required int height,
+    required int width,
+  }) : super(url: url, height: height, width: width);
+
+  factory ImageModel.fromJson(Map<String, dynamic> json) => ImageModel(
+      url: json['url'],
+      height: json['height'],
+      width: json['width'],
+    );
+}
+
