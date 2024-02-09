c0fe2a6198e8478efabdf9edf89b62c150825aed
tortamque <90132962+tortamque@users.noreply.github.com>
2024-02-09 14:28:14 +0200

    Update repo

--- lib/core/shared/data/repository/search_repository_impl.dart
+++ lib/core/shared/data/repository/search_repository_impl.dart
@@ -4,6 +4,7 @@ import 'package:dio/dio.dart';
 import 'package:musiclum/core/resources/data_state.dart';
 import 'package:musiclum/core/sensitive.dart';
 import 'package:musiclum/core/shared/data/data_sources/remote/api_service.dart';
+import 'package:musiclum/core/shared/data/models/album_cover_model.dart';
 import 'package:musiclum/core/shared/data/models/artist_model.dart';
 import 'package:musiclum/core/shared/domain/entities/album_entity.dart';
 import 'package:musiclum/core/shared/domain/entities/user_album_entity.dart';
@@ -69,6 +70,31 @@ class SearchRepositoryImpl implements SearchRepository{
   Future<DataState<AlbumEntity>> getAlbumDetails(String albumId) async {
     try{
       final response = await _apiService.getAlbumDetails(
+        albumId: albumId,
+        authToken: 'Authorization: Bearer $token'
+      );
+
+      if(response.response.statusCode == HttpStatus.ok){
+        return DataSuccess(response.data);
+      } else{
+        return DataFailed(
+          DioError(
+            error: response.response.statusMessage,
+            response: response.response,
+            type: DioErrorType.badResponse,
+            requestOptions: response.response.requestOptions
+          )
+        );
+      }
+    } on DioError catch(e){
+      return DataFailed(e);
+    }
+  }
+
+  @override
+  Future<DataState<List<AlbumCoverModel>>> getAlbumCover(String albumId) async {
+    try{
+      final response = await _apiService.getAlbumCover(
         albumId: albumId,
         authToken: 'Authorization: Bearer $token'
       );
--- lib/core/shared/domain/repository/search_repository.dart
+++ lib/core/shared/domain/repository/search_repository.dart
@@ -1,4 +1,5 @@
 import 'package:musiclum/core/resources/data_state.dart';
+import 'package:musiclum/core/shared/domain/entities/album_cover_entity.dart';
 import 'package:musiclum/core/shared/domain/entities/album_entity.dart';
 import 'package:musiclum/core/shared/domain/entities/artist_entity.dart';
 import 'package:musiclum/core/shared/domain/entities/user_album_entity.dart';
@@ -7,4 +8,5 @@ abstract class SearchRepository{
   Future<DataState<List<ArtistEntity>>> searchArtists(String query);
   Future<DataState<List<UserAlbumEntity>>> getArtistAlbums(String artistId);
   Future<DataState<AlbumEntity>> getAlbumDetails(String albumId);
+  Future<DataState<List<AlbumCoverEntity>>> getAlbumCover(String albumId);
 }
